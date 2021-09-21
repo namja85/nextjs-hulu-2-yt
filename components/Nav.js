2b18e6d22e65321c9a0e953885154ce598291223
namja85 <60639118+namja85@users.noreply.github.com>
2021-09-21 23:58:55 +0900

    add results

--- components/Nav.js
+++ components/Nav.js
@@ -1,19 +1,23 @@
-import requests from "../utils/requests";
+import requests from '../utils/requests';
+import { useRouter } from 'next/router';
 
 const Nav = () => {
+  const router = useRouter();
+
   return (
-    <nav className="relative">
-      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-scroll last:pr-24 scrollbar-hide">
+    <nav className='relative'>
+      <div className='flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-scroll last:pr-24 scrollbar-hide'>
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
+            onClick={() => router.push(`/?genre=${key}`)}
             key={key}
-            className="cursor-pointer transition duration-100 transition hover:scale-125 hover:text-white active:text-red-500"
+            className='cursor-pointer transition duration-100 transition hover:scale-125 hover:text-white active:text-red-500'
           >
             {title}
           </h2>
         ))}
       </div>
-      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12"></div>
+      <div className='absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12'></div>
     </nav>
   );
 };
